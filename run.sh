60394c22fb91941d8c94354ca3e75a533d502687
Pavel Antonov <pehas.design@gmail.com>
2020-11-10 15:50:43 +0300

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git add .
-git commit -m "test"
-git push
-docker exec -w //data nodered npm install --save git+https://github.com/arnfox/node-red-contrib-google-oauth2.git
-docker restart nodered
